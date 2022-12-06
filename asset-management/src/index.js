9bf3534c4689e97c6a9806642532065ee5d5eb56
emanuelefavero <emafaux89@gmail.com>
2022-12-06 01:57:50 +0100

    add loading css in asset-management

--- asset-management/src/index.js
+++ asset-management/src/index.js
@@ -1,9 +1,11 @@
+import './style.css'
 import { myName } from './myName'
 
 function component() {
   const element = document.createElement('div')
 
   element.innerHTML = myName('John')
+  element.classList.add('hello')
 
   return element
 }
